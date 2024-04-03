7783c562013d9d14bebe1f93b35abbaf1801b467
codesbyarif <codeswitharif@gmail.com>
2024-04-03 21:43:46 +0700

    aplikasi sederhana

---
+++ src/main/java/com/mycompany/aplikasisederhana/AplikasiSederhana.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplikasisederhana;
+
+/**
+ *
+ * @author Nadiva
+ */
+public class AplikasiSederhana {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
